de5765955e5be6fa925efce87d9ad0922dc1e231
Mouhab El-Banna <57503529+Mouhab-dev@users.noreply.github.com>
2021-09-29 06:18:04 +0200

    Update SiteReview.py

--- SiteReview.py
+++ SiteReview.py
@@ -7,8 +7,10 @@ import time
 
 import pytesseract
 import requests
+import requests.auth
 from PIL import Image
-
+from getpass import getpass
+from requests.exceptions import ProxyError
 
 # defining an exception for wrong top-level domain error
 class WrongTLD(Exception):
@@ -18,12 +20,18 @@ class WrongTLD(Exception):
 
 
 class SiteReview:
+    def __init__(self,username,password):
+        self.proxyDict = { 
+                "http"  : 'http://'+username+':'+password+'@proxy_ip:proxy_port'
+                "https"  : 'https://'+username+':'+password+'@proxy_ip:proxy_port'
+        }
+    
     # headers for requests
     headers = {
         'authority': 'sitereview.bluecoat.com',
         'accept': 'application/json, text/plain, */*',
         'x-xsrf-token': 'ce6e0505-ed4b-43ef-9624-9a5ad3b70f27',
-        'accept-language': 'ru',
+        'accept-language': 'en',
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
         'content-type': 'application/json; charset=UTF-8',
         'origin': 'https://sitereview.bluecoat.com',
@@ -43,17 +51,16 @@ class SiteReview:
         'sec-fetch-mode': 'no-cors',
         'sec-fetch-dest': 'image',
         'referer': 'https://sitereview.bluecoat.com/',
-        'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        'accept-language': 'en-US;q=0.8,en;q=0.7',
         'cookie': 'JSESSIONID=FD00E4699288139952D7B9CF25DC6F61; XSRF-TOKEN=ce6e0505-ed4b-43ef-9624-9a5ad3b70f27; __utma=265713933.443691065.1605382266.1605382266.1605382266.1; __utmc=265713933; __utmz=265713933.1605382266.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)',
     }
 
     # get decoded captcha answer
     def __get_captcha_base64(self):
         epoch_timestamp = str(calendar.timegm(time.gmtime()) * 1000)
-        captcha_url = 'https://sitereview.bluecoat.com/resource/captcha.jpg?%s' % (epoch_timestamp)
+        captcha_url = 'http://sitereview.bluecoat.com/resource/captcha.jpg?%s' % (epoch_timestamp)
         local_filename = 'captcha.jpg'
-        r = requests.get(captcha_url, headers=self._headers, stream=True)
-
+        r = requests.get(captcha_url, headers=self._headers, stream=True, proxies=self.proxyDict)
         with open(local_filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk:
@@ -79,25 +86,34 @@ class SiteReview:
     # send captcha answer
     def __captcha_recognize(self):
         captcha = self.__get_captcha_base64()
-        response = requests.get(f'https://sitereview.bluecoat.com/resource/captcha-request/{captcha}',
-                                headers=self._headers)
+        response = requests.get(f'http://sitereview.bluecoat.com/resource/captcha-request/{captcha}',
+                                headers=self._headers, proxies=self.proxyDict)
 
     # if names_only == True -> get only category names from answer dictionaries
     def __get_names_only(self, categories):
         answers = []
         for answer in categories:
             category_name = answer['name']
-            answers.append(category_name[category_name.find("(") + 1:category_name.find(")")])
+            answers.append(category_name)
         return answers
 
-    def get_category(self, url: str, names_only=False, is_again=False) -> list:
+    def get_category(self, url: str, names_only=True, is_again=False) -> list:
         data = self.__get_data(url)
-        response = requests.post('https://sitereview.bluecoat.com/resource/lookup', headers=self.headers, data=data)
+        response = requests.post('http://sitereview.bluecoat.com/resource/lookup', headers=self.headers, data=data, proxies=self.proxyDict)
+        #print(response.text)
         if response.status_code == 200:
             if names_only:
                 return self.__get_names_only(json.loads(response.text)['categorization'])
             return json.loads(response.text)['categorization']
-        if response.status_code == 422:
+        # Check for captcha check
+        if json.loads(response.text)['errorType'] == 'captcha':
+            #print('[!] '+json.loads(response.text)['errorMessage'])
+            print('[*] Solving Captcha... | ', end='')
+            self.__captcha_recognize()
+            return self.get_category(url, is_again=True)
+        # Check for badurl (WrongTLD)
+        if json.loads(response.text)['errorType'] == 'badurl':
+            #print('[!] '+json.loads(response.text)['errorMessage'])
             raise WrongTLD
         # this condition is required for every 10th request
         elif not is_again:
@@ -108,3 +124,38 @@ class SiteReview:
             time.sleep(5)
             self.__captcha_recognize()
             return self.get_category(url, is_again=True)
+
+
+# Logic
+try:
+    user = input('Enter your username: ')
+    passwd = getpass('Enter your password: ')
+    categorizer = SiteReview(username=user,password=passwd)
+
+    blocked_categories = ['Malicious Outbound Data/Botnets',
+                            'Malicious Sources/Malnets',
+                            'Phishing','Pornography',
+                            'Potentially Unwanted Software',
+                            'Spam','Suspicious']
+
+    # Read from File
+    with open('iocs.txt','r') as iocs_file:
+        with open('iocs_result.txt', 'w') as iocs_result:
+            for ioc in iocs_file:
+                ioc = ioc.strip().replace('[.]','.').replace('[:]',':').replace('hxxp','http')
+                try:
+                    cat = categorizer.get_category(ioc, names_only=True)
+                    print(ioc, cat)
+                    if cat[0] not in blocked_categories:
+                        #print(f'{ioc}:{cat}')
+                        iocs_result.write(f'{ioc}\n')
+                except WrongTLD:
+                    print(f'[!] Wrong domain for {ioc}')
+                    #iocs_result.write(f'[!] Wrong domain for {ioc}\n')
+                except ProxyError as e:
+                    print(e)
+                    break
+    print('Please, Check the output file called ./iocs_result.txt')
+except KeyboardInterrupt:
+    print('\n[!] Terminating Bluecoat Checker Script')
+    exit(1)
